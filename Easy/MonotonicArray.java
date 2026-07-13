8b914f8df19b5b2a7998315bbc26bf1363457518
Palak Singh <palaksingh9503@gmail.com>
2026-07-13 09:14:22 -0700

    Added LeetCode 896 - Monotonic Array

---
+++ Easy/MonotonicArray.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+public class MonotonicArray {
+
+    public static boolean isMonotonic(int[] nums) {
+
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] > nums[i + 1]) {
+                increasing = false;
+            }
+
+            if (nums[i] < nums[i + 1]) {
+                decreasing = false;
+            }
+        }
+
+        return increasing || decreasing;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 2, 3};
+
+        System.out.println(isMonotonic(nums));
+    }
+}
