dc607bc328677c1694e71de48978241e32f0ffbf
Palak Singh <palaksingh9503@gmail.com>
2026-08-06 05:36:57 -0700

    Added this solution of leetcode

---
+++ Easy/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            LinkedList<Integer> level = new LinkedList<>();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = queue.poll();
+
+                if (leftToRight)
+                    level.addLast(node.val);
+                else
+                    level.addFirst(node.val);
+
+                if (node.left != null)
+                    queue.offer(node.left);
+
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+
+            result.add(level);
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(zigzagLevelOrder(root));
+    }
+}
