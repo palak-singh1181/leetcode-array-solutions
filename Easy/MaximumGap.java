62b48635c8e05042c145e619f73eba878b7bf231
Palak Singh <palaksingh9503@gmail.com>
2026-08-15 11:06:03 -0700

    Added this solution of leetcode

---
+++ Easy/MaximumGap.java
@@ -0,0 +1,79 @@
+
+public class MaximumGap {
+
+    public static int maximumGap(int[] nums) {
+
+        int n = nums.length;
+
+        if (n < 2)
+            return 0;
+
+        int min = nums[0];
+        int max = nums[0];
+
+        // Find minimum and maximum
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        if (min == max)
+            return 0;
+
+        // Bucket size
+        int bucketSize = Math.max(1, (max - min) / (n - 1));
+
+        // Number of buckets
+        int bucketCount = (max - min) / bucketSize + 1;
+
+        int[] bucketMin = new int[bucketCount];
+        int[] bucketMax = new int[bucketCount];
+
+        boolean[] used = new boolean[bucketCount];
+
+        // Put elements into buckets
+        for (int num : nums) {
+
+            int index = (num - min) / bucketSize;
+
+            if (!used[index]) {
+
+                bucketMin[index] = num;
+                bucketMax[index] = num;
+
+                used[index] = true;
+
+            } else {
+
+                bucketMin[index] = Math.min(bucketMin[index], num);
+                bucketMax[index] = Math.max(bucketMax[index], num);
+            }
+        }
+
+        int maxGap = 0;
+        int previousMax = min;
+
+        // Find maximum gap between buckets
+        for (int i = 0; i < bucketCount; i++) {
+
+            if (!used[i])
+                continue;
+
+            maxGap = Math.max(
+                    maxGap,
+                    bucketMin[i] - previousMax
+            );
+
+            previousMax = bucketMax[i];
+        }
+
+        return maxGap;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 6, 9, 1};
+
+        System.out.println(maximumGap(nums));
+    }
+}
