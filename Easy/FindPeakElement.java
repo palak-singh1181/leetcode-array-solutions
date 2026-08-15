220331524de4b660e022bc6650c7080af43c55f7
Palak Singh <palaksingh9503@gmail.com>
2026-08-15 11:08:29 -0700

    Added this solution of leetcode

---
+++ Easy/FindPeakElement.java
@@ -0,0 +1,34 @@
+public class FindPeakElement {
+
+    public static int findPeakElement(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+
+                right = mid;
+
+            } else {
+
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 1};
+
+        int result = findPeakElement(nums);
+
+        System.out.println("Peak index: " + result);
+        System.out.println("Peak value: " + nums[result]);
+    }
+}
