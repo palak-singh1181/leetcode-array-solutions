dc993b1d5972b9cd9b23bbc8385dd13eacaa2d8b
Palak Singh <palaksingh9503@gmail.com>
2026-08-24 07:07:28 -0700

    Added this solution of leetcode

---
+++ Easy/HouseRobberII.java
@@ -0,0 +1,40 @@
+
+public class HouseRobberII {
+
+    public static int rob(int[] nums) {
+
+        int n = nums.length;
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        int case1 = robRange(nums, 0, n - 2);
+        int case2 = robRange(nums, 1, n - 1);
+
+        return Math.max(case1, case2);
+    }
+
+    private static int robRange(int[] nums, int start, int end) {
+
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = start; i <= end; i++) {
+
+            int current = Math.max(prev1, prev2 + nums[i]);
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 1};
+
+        System.out.println(rob(nums));
+    }
+}
