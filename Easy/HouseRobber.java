bd64733cebc543c4ad63aa391295ac607927c5ab
Palak Singh <palaksingh9503@gmail.com>
2026-08-24 07:04:19 -0700

    Added this solution of leetcode

---
+++ Easy/HouseRobber.java
@@ -0,0 +1,25 @@
+public class HouseRobber {
+
+    public static int rob(int[] nums) {
+
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int money : nums) {
+
+            int current = Math.max(prev1, prev2 + money);
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 9, 3, 1};
+
+        System.out.println(rob(nums));
+    }
+}
