a2df01b36736d16649ba7a7f8e12e9f073f34c56
Palak Singh <palaksingh9503@gmail.com>
2026-08-29 11:05:29 -0700

    Added this solution of leetcode

---
+++ Medium/HappyNumber.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class HappyNumber {
+
+    public static boolean isHappy(int n) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        while (n != 1) {
+
+            if (set.contains(n)) {
+                return false;
+            }
+
+            set.add(n);
+
+            n = sumOfSquares(n);
+        }
+
+        return true;
+    }
+
+    public static int sumOfSquares(int n) {
+
+        int sum = 0;
+
+        while (n > 0) {
+
+            int digit = n % 10;
+            sum += digit * digit;
+            n = n / 10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 19;
+
+        System.out.println(isHappy(n));
+    }
+}
