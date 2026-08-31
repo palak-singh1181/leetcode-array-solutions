b5dd75fea7a6845924bc4f55f93ae3b74400013f
Palak Singh <palaksingh9503@gmail.com>
2026-08-31 10:24:34 -0700

    Added this solution of leetcode

---
+++ Medium/MoveZeroes.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+
+        int index = 0;
+
+        // Move non-zero elements to the front
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        // Fill remaining positions with zero
+        while (index < nums.length) {
+            nums[index] = 0;
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 1, 0, 3, 12};
+
+        moveZeroes(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
