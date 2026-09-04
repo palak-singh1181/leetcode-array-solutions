866a5551e97f7c8a35a97b4b16786101b472e0f3
Palak Singh <palaksingh9503@gmail.com>
2026-09-04 10:50:38 -0700

    Added this solution of leetcode

---
+++ Easy/KthSmallestElementInSortedMatrix.java
@@ -0,0 +1,66 @@
+
+public class KthSmallestElementInSortedMatrix {
+
+    public static int kthSmallest(int[][] matrix, int k) {
+
+        int n = matrix.length;
+
+        int low = matrix[0][0];
+        int high = matrix[n - 1][n - 1];
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            int count = countLessEqual(matrix, mid);
+
+            if (count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    private static int countLessEqual(int[][] matrix, int target) {
+
+        int n = matrix.length;
+
+        int row = n - 1;
+        int col = 0;
+
+        int count = 0;
+
+        while (row >= 0 && col < n) {
+
+            if (matrix[row][col] <= target) {
+
+                count += row + 1;
+                col++;
+
+            } else {
+
+                row--;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 5, 9},
+            {10, 11, 13},
+            {12, 13, 15}
+        };
+
+        int k = 8;
+
+        int answer = kthSmallest(matrix, k);
+
+        System.out.println(answer);
+    }
+}
