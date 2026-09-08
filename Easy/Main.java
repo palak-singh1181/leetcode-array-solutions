30fcdcfd1e07597c14e86001ae1b8172d09b1254
Palak Singh <palaksingh9503@gmail.com>
2026-09-08 08:42:29 -0700

    Added this solution

---
+++ Easy/Main.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Main {
+
+    public static int maxProfit(int k, int[] prices) {
+
+        int n = prices.length;
+
+        if (n == 0 || k == 0) {
+            return 0;
+        }
+
+        // If k is large enough, transactions can be unlimited.
+        if (k >= n / 2) {
+            int profit = 0;
+
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1]) {
+                    profit += prices[i] - prices[i - 1];
+                }
+            }
+
+            return profit;
+        }
+
+        int[] buy = new int[k + 1];
+        int[] sell = new int[k + 1];
+
+        Arrays.fill(buy, Integer.MIN_VALUE);
+
+        for (int price : prices) {
+
+            for (int transaction = 1; transaction <= k; transaction++) {
+
+                buy[transaction] = Math.max(
+                    buy[transaction],
+                    sell[transaction - 1] - price
+                );
+
+                sell[transaction] = Math.max(
+                    sell[transaction],
+                    buy[transaction] + price
+                );
+            }
+        }
+
+        return sell[k];
+    }
+
+    public static void main(String[] args) {
+
+        int k = 2;
+        int[] prices = {3, 2, 6, 5, 0, 3};
+
+        System.out.println(maxProfit(k, prices));
+    }
+}
