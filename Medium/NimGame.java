e98f910a71b13029b69804b1ee6d0a2dd55636a2
Palak Singh <palaksingh9503@gmail.com>
2026-09-10 10:08:14 -0700

    Added this solution of leetcode

---
+++ Medium/NimGame.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NimGame {
+
+    public static boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of stones: ");
+        int n = sc.nextInt();
+
+        boolean result = canWinNim(n);
+
+        System.out.println("Can you win? " + result);
+
+        sc.close();
+    }
+}
